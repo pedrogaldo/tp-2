49885120061b24a378db1afe57fddc1177dd7043
pedrogaldo <101400428+pedrogaldo@users.noreply.github.com>
2022-04-23 12:35:19 -0300

    Texto

---
+++ Assets/mayoriadeedad.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class mayoriadeedad : MonoBehaviour
+{
+    public int edad;
+    public Text texto;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (edad >= 18)
+        {
+            texto.text = "usted es mayor de edad";
+            Debug.Log("usted es mayor de edad");
+        }
+
+        else
+        {
+            texto.text = "usted es menor de edad";
+            Debug.Log("usted es menor de edad");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
