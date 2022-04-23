c5852a6730b4fb2e7d26bd79613bb2f202055efb
pedrogaldo <101400428+pedrogaldo@users.noreply.github.com>
2022-04-23 15:36:31 -0300

    Terminado

--- Assets/scripts/ValidarContrasenia.cs
+++ Assets/scripts/ValidarContrasenia.cs
@@ -7,10 +7,13 @@ public class ValidarContrasenia : MonoBehaviour
     string contraseniaCorrecta;
     string contraseniaUsuario;
     public Text IngresoUsuario;
+    public Text txtmsj;
+    public GameObject msjError;
     // Start is called before the first frame update
     void Start()
     {
         contraseniaCorrecta = "123123";
+        msjError.SetActive(false);
     }
 
     // Update is called once per frame
@@ -24,10 +27,14 @@ public class ValidarContrasenia : MonoBehaviour
         contraseniaUsuario = IngresoUsuario.text;
         if (contraseniaUsuario == contraseniaCorrecta)
         {
+            txtmsj.text = "bienvenido";
+            msjError.SetActive(true);
             Debug.Log("bienvenido");
         }
         else
         {
+            txtmsj.text = "contraseña incorrecta";
+            msjError.SetActive(true);
             Debug.Log("contraseña incorrecta");
         }
     }
