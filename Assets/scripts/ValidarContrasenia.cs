1e21c7ddb743674b4ee5e9f3b2b3b4c42b4f8413
pedrogaldo <101400428+pedrogaldo@users.noreply.github.com>
2022-04-23 15:18:04 -0300

    2

---
+++ Assets/scripts/ValidarContrasenia.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ValidarContrasenia : MonoBehaviour
+{
+    string contraseniaCorrecta;
+    string contraseniaUsuario;
+    public Text IngresoUsuario;
+    // Start is called before the first frame update
+    void Start()
+    {
+        contraseniaCorrecta = "123123";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void validarContrasenia ()
+    {
+        contraseniaUsuario = IngresoUsuario.text;
+        if (contraseniaUsuario == contraseniaCorrecta)
+        {
+            Debug.Log("bienvenido");
+        }
+        else
+        {
+            Debug.Log("contraseña incorrecta");
+        }
+    }
+}
